5c31e6d0a6da1afc481ecfd07d73a45f9a39d43b
dingjianjaja <dingjianjaja@qq.com>
2017-11-15 16:44:40 +0800

    注释--dingjian

--- SettingTableViewExample/SettingTableViewExample/Classes/AppDelegate.h
+++ SettingTableViewExample/SettingTableViewExample/Classes/AppDelegate.h
@@ -5,6 +5,20 @@
 //  Created by chensx on 17/3/10.
 //  Copyright © 2017年 customTableView. All rights reserved.
 //
+/**
+ *  .--,       .--,
+ * ( (  \.---./  ) )
+ *  '.__/o   o\__.'
+ *     {=  ^  =}
+ *      >  -  <
+ *     /       \
+ *    //       \\
+ *   //|   .   |\\
+ *   "'\       /'"_.-~^`'-.
+ *      \  _  /--'         `
+ *    ___)( )(___
+ *   (((__) (__)))
+ */
 
 #import <UIKit/UIKit.h>
 
